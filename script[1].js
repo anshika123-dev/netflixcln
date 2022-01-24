48898fc31803fa7627c01f144478afc09c19b8fc
anshika123-dev <84896831+anshika123-dev@users.noreply.github.com>
2022-01-24 16:54:06 +0530

    Add files via upload

---
+++ script[1].js
@@ -0,0 +1,19 @@
+// let acoordin= document.getElementsByClassName("faq__title");
+// for(let i=0;i<acoordin.length;i++){
+// acordin[i].addEventListener("click",function(){
+//  if(this.childNodes[1].classList.contains("fa-plus") ){
+//    this.childNodes[1].classList.remove("fa-plus");
+//    this.childNodes[1].classList.add("fa-tiems");
+//   }else{
+//  this.childNodes[1].classList.add("fa-tiems");
+//  this.childNodes[1].classList.remove("fa-plus");
+//  }
+//  let content=this.nextElementSibling;
+//  if(content.style.maxHeight){
+//    content.style.maxHeight=null;
+//  }else{
+//     content.style.maxHeight=content.scrollHeight + "px";
+//   }
+
+//  });
+//  }
